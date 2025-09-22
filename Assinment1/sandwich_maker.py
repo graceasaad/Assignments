f23880ea71a96f12ad1b42d132a1fa82befbf7eb
graceasaad <gasaad@charlotte.edu>
2025-09-22 14:57:24 -0400

    checking resources

---
+++ Assinment1/sandwich_maker.py
@@ -0,0 +1,19 @@
+class SandwichMaker:
+    def __init__(self, resources):
+        self.machine_resources = resources
+
+    def check_resources(self, ingredients):
+        for item, amount in ingredients.items():
+            if self.resources[item] < amount:
+                print(f"There isn't enough {item}.")
+                return False
+        return True
+    def make_sandwich(self, s_size, order_ingredients):
+        for item, amount in ingredients.items():
+            self.resources[item]-= amount
+            print("Order complete. Enjoy!")
+
+    def report(self):
+        print(f"Bread has {self.resources['bread']} slices left.")
+        print(f"Ham has {self.resources['ham']} slices left.")
+        print(f"Cheese has {self.resources['cheese']} slices left.")
