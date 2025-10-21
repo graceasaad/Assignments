7ec5bae7569e127544b09d9b98c3565197d42a92
graceasaad <gasaad@charlotte.edu>
2025-10-20 21:14:07 -0400

    commit 3

--- main.py
+++ main.py
@@ -23,3 +23,4 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/item/{item_id}")
 def update_item(item_id: int, item: Item ):
     return {"item_name": item.name, "item_id": item_id}
+
